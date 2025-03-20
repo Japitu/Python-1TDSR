bbecb470f8763b678666b58e3a3ca1aadc05d3bb
Felipe Ishii <felipeishii55@gmail.com>
2025-03-19 22:42:54 -0300

    feito atividade 7 e 8

---
+++ Aula atividades 19.03/at8.py
@@ -0,0 +1,7 @@
+camiseta = int(input("Informe a quantidade de camisetas: "))
+calça = int(input("Informe a quantidade de calças: "))
+sapato = int(input("Informe a quantidade de sapatos: "))
+
+maneiras = camiseta * calça * sapato
+
+print("Com essas roupas temos", maneiras, "possibilidades de roupas.")
