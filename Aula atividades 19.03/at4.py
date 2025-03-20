797b2009a109d4f9fa3872c4f0ccb78fcdd7e1e9
Felipe Ishii <felipeishii55@gmail.com>
2025-03-19 22:55:44 -0300

    atualizado

--- Aula atividades 19.03/at4.py
+++ Aula atividades 19.03/at4.py
@@ -3,11 +3,15 @@ n2 = int(input("Informe outro número: "))
 
 
 soma = n1 + n2
-mult = n1 * n2
+prod = n1 * n2
 divIn = n1 // n2
 rest = n1 % n2 
 
-print("Soma dos 2 números: ", soma)
-print("Multiplicação dos 2 números ", mult)
-print("Divisão Inteira dos 2 números ", divIn)
-print("Resto da divisão dos 2 números ", rest)
+print("Soma dos 2 números: ")
+print(f"{n1} + {n2} = {soma}")
+print("Multiplicação dos 2 números: ")
+print(f"{n1} * {n2} = {prod}")
+print("Divisão Inteira dos 2 números: ")
+print(f"{n1} // {n2} = {divIn}")
+print("Resto da divisão dos 2 números: ")
+print(f"{n1} % {n2} = {rest}")
