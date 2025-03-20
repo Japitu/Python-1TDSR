c7f0b8f0e1ad1747ee5a388bfb0c1e6a7430fbf9
Felipe Ishii <felipeishii55@gmail.com>
2025-03-19 22:57:35 -0300

    até Atividade 4

--- Aula atividades 19.03/at7.py
+++
@@ -1,7 +0,0 @@
-n1 = int(input("Informe um número de 0 a 99: "))
-
-if n1 >= 0 and n1 <= 99:
-    dez = n1 // 10
-    unid = n1 - dez * 10
-    print("A dezena é: ", dez)
-    print("A unidade é: ", unid)
--- Aula atividades 19.03/at8.py
+++
@@ -1,7 +0,0 @@
-camiseta = int(input("Informe a quantidade de camisetas: "))
-calça = int(input("Informe a quantidade de calças: "))
-sapato = int(input("Informe a quantidade de sapatos: "))
-
-maneiras = camiseta * calça * sapato
-
-print("Com essas roupas temos", maneiras, "possibilidades de roupas.")
